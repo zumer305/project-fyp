a6e96b3ceff2408d249f816a652ea55b97b54429
zumer305 <zumerniaz305@gmail.com>
2025-10-13 00:57:53 +0700

    push

--- models/user.js
+++ models/user.js
@@ -7,8 +7,6 @@ const userSchema=new Schema({
         type:String,
         required:true
     },
-
-
 })
 User.plugin(passportLocalMongoose);//automatically (user,pass(hash or salt))
 module.exports=mongoose.model("user",userSchema);
