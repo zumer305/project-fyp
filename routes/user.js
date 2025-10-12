0b2eb992fbb57032d8b7d6ff0650753a9d36fcee
zumer305 <zumerniaz305@gmail.com>
2025-10-13 02:30:04 +0700

    push

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 
 router.get("/signup",(req,res)=>{
-    res.send("form");
+    res.render("users/signup.ejs");
 });
 
 module.exports=router;
