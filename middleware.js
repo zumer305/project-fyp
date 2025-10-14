08e0f913c49bb019184cdf5b512f913770196a52
zumer305 <zumerniaz305@gmail.com>
2025-10-14 21:48:09 +0700

    push

--- middleware.js
+++ middleware.js
@@ -2,9 +2,18 @@
 
     module.exports.isLoggedIn=(req,res,next)=>{
          if(!req.isAuthenticated()){ //khudi auto pr check krly ga authenticate
+            req.session.redirectUrl=req.originalUrl;  
         req.flash("error","you must be logged in to create listing!");
         return res.redirect("/login");
     }
     next();
 
+    }
+
+
+    module.exports.saveRedirectUrl=(req,res,next)=>{
+        if(req.session.redirectUrl){
+            res.locals.redirectUrl=req.session.redirectUrl;
+        }
+        next();
     }
