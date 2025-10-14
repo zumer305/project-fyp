a18f917ef95919a28326393c9bdf6219a61f3b49
zumer305 <zumerniaz305@gmail.com>
2025-10-15 00:02:15 +0700

    push

--- routes/review.js
+++ routes/review.js
@@ -4,15 +4,16 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const Review=require("../models/review.js");//import file listing
 const Listing=require("../models/listing.js");//import file listing
-const{validateReview}=require("../middleware.js");
+const{validateReview,isLoggedIn}=require("../middleware.js");
 
 
 
 
 //reviews(POST review ROUTE)
-router.post("/",validateReview,wrapAsync( async (req, res) => {
+router.post("/",isLoggedIn,validateReview,wrapAsync( async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author=req.user._id;
 
     listing.reviews.push(newReview);
 
