aabb6e32ca0b76763bce3bccf2da8effd5c747da
zumer305 <zumerniaz305@gmail.com>
2025-11-13 22:42:39 +0500

    update

--- routes/api/groups.js
+++ routes/api/groups.js
@@ -6,5 +6,7 @@ const ctrl = require("../../controllers/api/groupsController.js");
 router.post("/", requireAuth, ctrl.create);
 router.post("/:id/join", requireAuth, ctrl.join);
 router.get("/:id/messages", requireAuth, ctrl.messages);
+router.get("/:id/plan", requireAuth, ctrl.plan);
+router.get("/:id/expenses", requireAuth, ctrl.expenses);
 
 module.exports = router;
